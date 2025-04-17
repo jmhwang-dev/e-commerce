4d0c7ed9b63b17a11d3fe7fe43b4709dcd911388
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-04-17 11:54:02 +0900

    Commented out the Java configuration

--- hdfs/hadoop-env.sh
+++ hdfs/hadoop-env.sh
@@ -1,14 +1,14 @@
-OS=$(uname)
+# OS=$(uname)
 
-if [ "$OS" = "Darwin" ]; then
-    # brew install openjdk@8
-    export JAVA_HOME="/usr/local/opt/openjdk@8"
-elif [ "$OS" = "Linux" ]; then
-    export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-else
-    echo "JAVA_HOME is not set: Unknown operating system"
-fi
+# if [ "$OS" = "Darwin" ]; then
+#     # brew install openjdk@8
+#     export JAVA_HOME="/usr/local/opt/openjdk@8"
+# elif [ "$OS" = "Linux" ]; then
+#     export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+# else
+#     echo "JAVA_HOME is not set: Unknown operating system"
+# fi
+# export JAVA_HOME=$JAVA_HOME
 
-export JAVA_HOME=$JAVA_HOME
 export HADOOP_HOME=/opt/hadoop-3.4.1
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
