b7009202660bf011011fd1f9e2e6af8bf7900f32
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-04-17 15:05:44 +0900

    rename env name: HADOOP_HOME

--- hadoop-3.4.1/setup.sh
+++ hadoop-3.4.1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HADOOP_INSTALL_DIR="/opt/hadoop-3.4.1"
+HADOOP_HOME="/opt/hadoop-3.4.1"
 
 OS_TYPE=$(uname)
 
@@ -16,17 +16,17 @@ fi
 # Environment variable settings
 HADOOP_ENV="
 # >>> Hadoop Environment >>>
-export HADOOP_INSTALL_DIR=$HADOOP_INSTALL_DIR
-export PATH=\$PATH:\$HADOOP_INSTALL_DIR/bin:\$HADOOP_INSTALL_DIR/sbin
+export HADOOP_HOME=$HADOOP_HOME
+export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin
 # <<< Hadoop Environment <<<
 "
 
 # Prevent duplication
-if grep -q "HADOOP_INSTALL_DIR" "$PROFILE_FILE"; then
-  echo "HADOOP_INSTALL_DIR is already set in $PROFILE_FILE."
+if grep -q "HADOOP_HOME" "$PROFILE_FILE"; then
+  echo "HADOOP_HOME is already set in $PROFILE_FILE."
 else
   echo "$HADOOP_ENV" >> "$PROFILE_FILE"
-  echo "Added HADOOP_INSTALL_DIR settings to $PROFILE_FILE."
+  echo "Added HADOOP_HOME settings to $PROFILE_FILE."
   
   # Apply the changes immediately
   source "$PROFILE_FILE"
