374d7ff98d04074e5435638b33daef6d5a2e129a
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-04-27 11:33:25 +0900

    modify code to inference

--- translate/run_auto.py
+++ translate/run_auto.py
@@ -20,32 +20,32 @@ if __name__ == "__main__":
     prompts = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
     i = 0
-    batch_size_ = 10
+    batch_size_ = 8
     start = time.time()
-    with open(DST_PATH, 'a') as f:
-        while i < len(prompts):
-            try:
-                inference_start = time.time()
-                outputs = pipe(
-                    prompts[i:i+batch_size_],
-                    max_new_tokens=512,
-                    do_sample=False,
-                    batch_size=batch_size_
-                )
-                inference_end = time.time()
+    while i < len(prompts):
+        try:
+            inference_start = time.time()
+            outputs = pipe(
+                prompts[i:i+batch_size_],
+                max_new_tokens=512,
+                do_sample=False,
+                batch_size=batch_size_
+            )
+            inference_end = time.time()
 
+            with open(DST_PATH, 'a') as f:
                 for output in outputs:
                     kor = output[0]['generated_text'].split("<|im_start|>assistant\n")[-1].strip()
                     f.write(f"{kor}\n")
 
-                print(f"{i+batch_size_} / {len(prompts)} ... complete: {inference_end - inference_start}")
-                i += batch_size_
-                batch_size_ = min(batch_size_ + 5, 1024)  # 상한선 
+            print(f"{i+batch_size_} / {len(prompts)} ... complete: {inference_end - inference_start}")
+            i += batch_size_
+            batch_size_ = min(batch_size_ + 1, 1024)  # 상한선 
 
-            except torch.cuda.OutOfMemoryError as e:
-                print(f"[⚠️ OOM] batch_size={batch_size_} ↓ 줄임")
-                batch_size_ = max(1, batch_size_ - 1)
-                torch.cuda.empty_cache()
-                import gc; gc.collect()
+        except torch.cuda.OutOfMemoryError as e:
+            print(f"[⚠️ OOM] batch_size={batch_size_} ↓ 줄임")
+            batch_size_ = max(1, batch_size_ - 1)
+            torch.cuda.empty_cache()
+            import gc; gc.collect()
     end = time.time()
     print(f"Done: {end-start}")
--- translate/run_cpu.py
+++ translate/run_cpu.py
@@ -20,33 +20,33 @@ if __name__ == "__main__":
     prompts = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
     i = 0
-    batch_size_ = 200
+    batch_size_ = 50
     start = time.time()
-    with open(DST_PATH, 'a') as f:
-        while i < len(prompts):
-            try:
-                inference_start = time.time()
-                outputs = pipe(
-                    prompts[i:i+batch_size_],
-                    max_new_tokens=512,
-                    do_sample=False,
-                    batch_size=batch_size_
-                )
-                inference_end = time.time()
+    while i < len(prompts):
+        try:
+            inference_start = time.time()
+            outputs = pipe(
+                prompts[i:i+batch_size_],
+                max_new_tokens=512,
+                do_sample=False,
+                batch_size=batch_size_
+            )
+            inference_end = time.time()
 
+            with open(DST_PATH, 'a') as f:
                 for output in outputs:
                     kor = output[0]['generated_text'].split("<|im_start|>assistant\n")[-1].strip()
                     f.write(f"{kor}\n")
-                
-                print(f"{i+batch_size_} / {len(prompts)} ... complete: {inference_end - inference_start}")
-                i += batch_size_
-                batch_size_ = min(batch_size_ + 10, 2048)  # 상한선 
+            
+            print(f"{i+batch_size_} / {len(prompts)} ... complete: {inference_end - inference_start}")
+            i += batch_size_
+            batch_size_ = min(batch_size_ + 1, 2048)  # 상한선 
 
-            except RuntimeError as e:  # CPU에서의 메모리 부족 처리
-                print(f"[⚠️ RuntimeError] batch_size={batch_size_} ↓ 줄임")
-                batch_size_ = max(1, batch_size_ - 10)
-                import gc
-                gc.collect()
+        except RuntimeError as e:  # CPU에서의 메모리 부족 처리
+            print(f"[⚠️ RuntimeError] batch_size={batch_size_} ↓ 줄임")
+            batch_size_ = max(1, batch_size_ - 1)
+            import gc
+            gc.collect()
     end = time.time()
     print(f"Done: {end-start}")
     
