7e3235af97ba450ebff4909bc232ad62c3653b0c
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-05-09 17:52:45 +0900

    mkdir to save node info

--- hadoop/install/run.sh
+++ hadoop/install/run.sh
@@ -2,3 +2,6 @@ source ./hadoop/install/config.sh
 source ./hadoop/install/java.sh
 source ./hadoop/install/hadoop.sh
 source ./hadoop/install/profile.sh
+
+sudo mkdir -p /usr/local/hadoop_data/dfs/name
+sudo mkdir -p /usr/local/hadoop_data/dfs/data
