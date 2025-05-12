bb2482265d88715f54ea799fdcdc7818a9a29f20
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-05-12 15:05:06 +0900

    add script to infer sentiment using mp

---
+++ run_sentiment.py
@@ -0,0 +1,25 @@
+from inference import *
+from config import *
+
+import multiprocessing as mp
+
+
+def analyze_sentiment(dataset, device_, initial_batch_size_, dst_file_name_):
+    analyzor = get_sentiment_analyzer(device_, initial_batch_size_, dst_file_name_)
+    analyzor.set_input(dataset)
+    analyzor.run()
+
+if __name__ == "__main__":
+    dataset_path = os.path.join(ARTIFACT_DIR, 'inference', "trans-p2e.txt")
+    dataset = load_dataset(dataset_path)
+
+    chunk_size = len(dataset) // 2
+
+    worker_senti_gpu = mp.Process(target=analyze_sentiment, args=(dataset[:chunk_size], 'cuda', 300, 'senti_eng_cpu3_but_gpu.csv'))
+    worker_senti_gpu.start()
+
+    worker_senti_cpu = mp.Process(target=analyze_sentiment, args=(dataset[chunk_size:], 'cpu', 300, 'senti_eng_cpu3.csv'))
+    worker_senti_cpu.start()
+    
+    worker_senti_cpu.join()
+    worker_senti_gpu.join()
