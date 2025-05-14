47dccbe663dc0654a988dc4a02f8a53d61938271
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-05-14 15:54:08 +0900

    add condition to skip making dir for node if client want to use hdfs

--- hadoop/install/run.sh
+++ hadoop/install/run.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 source ./hadoop/install/config.sh
 source ./hadoop/install/java.sh
 source ./hadoop/install/hadoop.sh
 source ./hadoop/install/profile.sh
 
-sudo mkdir -p /mnt/hadoop/dfs/name
-sudo mkdir -p /mnt/hadoop/dfs/data
-sudo chown -R jmhwang:jmhwang /mnt/hadoop
+HOSTNAME=$(hostname)
+
+if [[ "$HOSTNAME" == "mini-pc" || $HOSTNAME =~ ^raspberrypi ]]; then
+    sudo mkdir -p /mnt/hadoop/dfs/name
+    sudo mkdir -p /mnt/hadoop/dfs/data
+    sudo chown -R jmhwang:jmhwang /mnt/hadoop
+    echo "[DONE] HDFS cluster has configured."
+fi
+
+echo "[DONE] HDFS has configured."
