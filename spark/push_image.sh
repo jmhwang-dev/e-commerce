32c073b40b8ed1786fcac12fc7b5af5cf2c34b3b
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-05-27 17:48:40 +0900

    push spark_image to ghrc

---
+++ spark/push_image.sh
@@ -0,0 +1,11 @@
+# login
+echo <token> | docker login ghcr.io -u jmhwang-dev --password-stdin
+
+# ghcr 용 태그 생성
+docker tag my-spark-hadoop:3.5.5-arm64 ghcr.io/jmhwang-dev/spark-hadoop:3.5.5-arm64
+
+# push
+docker push ghcr.io/jmhwang-dev/spark-hadoop:3.5.5-arm64
+
+# 실패시
+docker logout ghcr.io
