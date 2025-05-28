3dc339f6f2bd0704b41e17af95739cee37205b03
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-05-28 16:59:40 +0900

    modify to push spark image for amd64

--- spark/push_image.sh
+++ spark/push_image.sh
@@ -3,9 +3,11 @@ echo <token> | docker login ghcr.io -u jmhwang-dev --password-stdin
 
 # ghcr 용 태그 생성
 docker tag my-spark-hadoop:3.5.5-arm64 ghcr.io/jmhwang-dev/spark-hadoop:3.5.5-arm64
+docker tag my-spark-hadoop:3.5.5-amd64 ghcr.io/jmhwang-dev/spark-hadoop:3.5.5-amd64
 
 # push
 docker push ghcr.io/jmhwang-dev/spark-hadoop:3.5.5-arm64
+docker push ghcr.io/jmhwang-dev/spark-hadoop:3.5.5-amd64
 
 # 실패시
 docker logout ghcr.io
