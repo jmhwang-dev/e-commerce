6d462c668a9c6c7461f343f66adfcebdae3841e6
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-06-02 12:36:42 +0900

    seperate scripts: install, backup

--- scripts/minio/backup.sh
+++ scripts/minio/backup.sh
@@ -1,8 +1,3 @@
-# install
-curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
-chmod +x mc
-sudo mv mc /usr/local/bin/
-
 # connect
 mc alias set localminio http://localhost:9000 minioadmin minioadmin
 
