85635d6445c12cd2db461a4672501264337cdd88
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-06-28 13:10:10 +0900

    use jupyter to run pyspark in dev container

---
+++ jobs/spark/jupyter_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from pyspark.sql import SparkSession
+
+
+# In[4]:
+
+
+import pyspark
+pyspark.__version__
+
+
+# In[11]:
+
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Simple S3A Test") \
+    .getOrCreate()
+
+try:
+    df = spark.read.csv("s3a://bronze/olist_customers_dataset.csv", header=True)
+    df.show()
+    print("✅ 읽기 성공!")
+except Exception as e:
+    print(f"❌ 오류: {e}")
+finally:
+    spark.stop()
+
+
+# In[12]:
+
+
+df.printSchema()
+
