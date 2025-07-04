9fbd47276e4632978f5c95507dfb54bc7bb9b4b8
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-07-04 16:28:58 +0900

    chore

--- scripts/docker/build-catalog.sh
+++ scripts/docker/build-catalog.sh
@@ -1,5 +1,5 @@
 DOCKER_BUILDKIT=1 docker build --no-cache \
-  -t my-rest-catalog:1.9.1 \
+  -t catalog:ice1.9.1-psql16 \
   --build-arg ICEBERG_VERSION=1.9.1 \
   --build-arg PG_JDBC_VERSION=42.7.3 \
   ./infra/iceberg-rest \
--- scripts/docker/build-lakehouse.sh
+++ scripts/docker/build-lakehouse.sh
@@ -1,5 +1,5 @@
 DOCKER_BUILDKIT=1 docker build --no-cache \
-  -t ecommerce-lakehouse:sp3.5.6-ice1.9.1 \
+  -t lakehouse:sp3.5.6-ice1.9.1 \
   --build-arg ICEBERG_VERSION=1.9.1 \
   --build-arg SPARK_VERSION=3.5.6 \
   --build-arg ICEBERG_VERSION=1.9.1 \
