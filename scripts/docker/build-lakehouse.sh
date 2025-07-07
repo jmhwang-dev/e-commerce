263158a3b8d6db38393e815b6aa183e42fd33226
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-07-07 11:25:02 +0900

    connect minio from spark

--- scripts/docker/build-lakehouse.sh
+++ scripts/docker/build-lakehouse.sh
@@ -6,5 +6,7 @@ DOCKER_BUILDKIT=1 docker build --no-cache \
   --build-arg ICEBERG_SPARK_RUNTIME_VERSION=3.5_2.12 \
   --build-arg SCALA_BINARY=2.12 \
   --build-arg JAVA_VERSION=17 \
+  --build-arg HADOOP_AWS_VERSION=3.3.4 \
+  --build-arg AWS_SDK_VERSION=1.12.262 \
   ./infra/spark \
 && docker image prune -f
