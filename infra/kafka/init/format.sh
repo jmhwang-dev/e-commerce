c7e680e66e52ea937b0b4ab4ab37ed078efd3dd3
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-07-17 16:18:10 +0900

    fix: chown from root to appuser to write meta.properties

--- infra/kafka/init/format.sh
+++ infra/kafka/init/format.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Formatting kafka1 with Cluster ID: ${CLUSTER_ID}"
+echo "[INIT] Fixing ownership..."
+chown -R appuser:appuser /var/lib/kafka/data
 
+echo "[INIT] Formatting with Cluster ID: ${CLUSTER_ID}"
 if [ ! -f /var/lib/kafka/data/meta.properties ]; then
   exec gosu appuser /opt/kafka/bin/kafka-storage.sh format \
     --cluster-id "${CLUSTER_ID}" \
