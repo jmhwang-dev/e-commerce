e3969757c21ee715bca0997b8719de35b21a9b4c
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-08-12 21:21:03 +0900

    fix(docker): remove unnecessary service: producer, kafak-init

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -2,6 +2,6 @@ CREATE USER iceberg WITH ENCRYPTED PASSWORD 'iceberg';
 CREATE DATABASE iceberg;
 ALTER DATABASE iceberg OWNER TO iceberg;
 
-CREATE USER superset WITH PASSWORD 'superset';
-CREATE DATABASE superset OWNER superset;
-ALTER SCHEMA public OWNER TO superset;
+-- CREATE USER superset WITH PASSWORD 'superset';
+-- CREATE DATABASE superset OWNER superset;
+-- ALTER SCHEMA public OWNER TO superset;
