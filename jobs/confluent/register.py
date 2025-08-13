bead6bbdad0ddb1c7c985a57c78b3e592740deec
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-08-13 15:58:12 +0900

    feat(confluent): register schemas for all raw

--- jobs/confluent/register.py
+++ jobs/confluent/register.py
@@ -1,12 +1,19 @@
 from service.init.confluent import *
+from pathlib import Path
+import os
 
 if __name__ == "__main__":
     # Load schemas (in prod: read from files or Git)
-    with open("infra/confluent/schemas/reviews.avsc", "r") as f:
-        reviews_schema = f.read()
-    
-    # Set compatibility first
-    set_compatibility("review")
-    
-    # Register
-    register_schema("review", reviews_schema)
+    SCHEMA_DIR = Path('infra/confluent/schemas')
+    for file_name in os.listdir(SCHEMA_DIR):
+        schema_path = SCHEMA_DIR / file_name
+        schema_name = schema_path.stem
+        
+        with open(schema_path, "r") as f:
+            schema = f.read()
+        
+        # Set compatibility first
+        set_compatibility(schema_name)
+        
+        # Register
+        register_schema(schema_name, schema)
--- scripts/confluent/check_schema.sh
+++ scripts/confluent/check_schema.sh
@@ -1,4 +1,13 @@
-REGISTRY_URL="${SCHEMA_REGISTRY_URL:-http://localhost:8082}"
-SUBJECT="review"
+#!/bin/bash
 
-curl -s "$REGISTRY_URL/subjects/$SUBJECT/versions/latest" | jq .
+REGISTRY_URL="${SCHEMA_REGISTRY_URL:-http://localhost:8082}"
+
+# 모든 subject 목록 가져오기
+subjects=$(curl -s "$REGISTRY_URL/subjects" | jq -r '.[]')
+
+# 각 subject에 대해 latest 스키마 출력
+for subject in $subjects; do
+  echo "Subject: $subject"
+  curl -s "$REGISTRY_URL/subjects/$subject/versions/latest" | jq .
+  echo "-------------------"
+done
