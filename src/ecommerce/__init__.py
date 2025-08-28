c175c06e954fef939da8233470b5126ddd7173d1
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-08-28 20:42:29 +0900

    chore: mv legacy codes

--- src/ecommerce/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import utils
-from . import inference
-
-__all__ = [
-    "utils",
-    "inference",
-]
--- src/ecommerce/inference/__init__.py
+++
@@ -1,9 +0,0 @@
-from .sentiment import SentimentAnalyzer, run_sentiment
-from .translate import Translator, run_translator
-
-__all__ = [
-    "SentimentAnalyzer",
-    "run_sentiment",
-    "Translator",
-    "run_translator",
-]
