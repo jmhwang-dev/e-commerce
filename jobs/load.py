013e47f75c9b959ece949945055f48b5fe3aeca7
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-09-04 20:27:36 +0900

    fix(loader): merge into one loader

---
+++ jobs/load.py
@@ -0,0 +1,29 @@
+from pyspark.sql.functions import col
+from pyspark.sql import DataFrame
+
+from service.common.topic import BronzeTopic, SilverTopic
+from service.utils.schema.registry_manager import SchemaRegistryManager
+from service.utils.schema.reader import AvscReader
+from service.utils.spark import get_spark_session, get_kafka_stream_df
+from service.pipeline.load import load_medallion_layer
+
+TARGET_TOPIC_NAMES = BronzeTopic.get_all_topics() + SilverTopic.get_all_topics()
+
+def process_micro_batch(micro_batch_df: DataFrame, batch_id: int):
+    load_medallion_layer(TARGET_TOPIC_NAMES, micro_batch_df, batch_id)
+
+if __name__ == "__main__":
+    spark_session = get_spark_session("LoadSilverJob")
+    client = SchemaRegistryManager._get_client(use_internal=True)
+    target_topics_stream_df = get_kafka_stream_df(spark_session, TARGET_TOPIC_NAMES)
+    base_stream_df = target_topics_stream_df.select(col("key").cast("string"), col("value"), col("topic"))
+
+    avsc_reader = AvscReader(TARGET_TOPIC_NAMES[0])
+    query = base_stream_df.writeStream \
+        .foreachBatch(process_micro_batch) \
+        .queryName("load_silver_layer") \
+        .option("checkpointLocation", f"s3a://{avsc_reader.s3_uri}/checkpoints") \
+        .trigger(processingTime="10 seconds") \
+        .start()
+        
+    query.awaitTermination()
---
+++ src/service/pipeline/load.py
@@ -0,0 +1,30 @@
+from typing import List
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
+
+from service.utils.schema.reader import AvscReader
+from service.utils.spark import get_decoded_stream_df
+
+def load_medallion_layer(target_topic_names: List[str], micro_batch_df:DataFrame, batch_id: int):
+    topics_in_batch = [row.topic for row in micro_batch_df.select("topic").distinct().collect()]
+    
+    print(f"Processing Batch ID: {batch_id}")
+    print(f"Topics in Batch: {topics_in_batch}")
+    print()
+    for topic_name in target_topic_names:
+        try:
+            avsc_reader = AvscReader(topic_name)
+            topic_df = micro_batch_df.filter(col("topic") == topic_name)
+            deserialized_df = get_decoded_stream_df(topic_df, avsc_reader.schema_str)        
+            record_count = deserialized_df.count()
+            if record_count > 0:
+                print(f"Writing {record_count} rows to Iceberg table: {avsc_reader.dst_table_identifier}")
+                deserialized_df.write \
+                    .format("iceberg") \
+                    .mode("append") \
+                    .saveAsTable(avsc_reader.dst_table_identifier)
+            else:
+                print(f"No records to write for topic {topic_name} in this batch.")
+
+        except Exception as e:
+            print(f"Error processing topic {topic_name} in batch {batch_id}: {e}")
