3104c15286673dee0c4aec7dc45140b88b39a299
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-09-04 20:52:56 +0900

    chore

--- jobs/load.py
+++ jobs/load.py
@@ -13,7 +13,7 @@ def process_micro_batch(micro_batch_df: DataFrame, batch_id: int):
     load_medallion_layer(TARGET_TOPIC_NAMES, micro_batch_df, batch_id)
 
 if __name__ == "__main__":
-    spark_session = get_spark_session("LoadSilverJob")
+    spark_session = get_spark_session("LoadAllTopicsJob")
     client = SchemaRegistryManager._get_client(use_internal=True)
     target_topics_stream_df = get_kafka_stream_df(spark_session, TARGET_TOPIC_NAMES)
     base_stream_df = target_topics_stream_df.select(col("key").cast("string"), col("value"), col("topic"))
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     avsc_reader = AvscReader(TARGET_TOPIC_NAMES[0])
     query = base_stream_df.writeStream \
         .foreachBatch(process_micro_batch) \
-        .queryName("load_silver_layer") \
+        .queryName("load_all_topics") \
         .option("checkpointLocation", f"s3a://{avsc_reader.s3_uri}/checkpoints") \
         .trigger(processingTime="10 seconds") \
         .start()
