abbfee03ce781717e2539424afd6e7d45bcf1494
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-09-06 13:45:11 +0900

    fix(query): add thrift-server

--- scripts/utils/docker/down.sh
+++ scripts/utils/docker/down.sh
@@ -3,4 +3,5 @@ docker compose \
     -f docker-compose.override.yml \
     -f docker-compose.pipeline.yml \
     -f docker-compose.inference.yml \
+    -f docker-compose.thrift.yml \
     down -v --remove-orphans
--- scripts/utils/docker/up.sh
+++ scripts/utils/docker/up.sh
@@ -3,4 +3,5 @@ docker compose \
     -f docker-compose.override.yml \
     -f docker-compose.pipeline.yml \
     -f docker-compose.inference.yml \
+    -f docker-compose.thrift.yml \
     up -d --force-recreate
