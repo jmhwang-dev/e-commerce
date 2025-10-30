5b2df41a38cf520a73efee3a19e5334d4b5645e5
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-10-30 13:48:15 +0900

    chore: add debug mode and modify app name

--- jobs/stream.py
+++ jobs/stream.py
@@ -12,7 +12,9 @@ LOGGER = get_logger(__name__, '/opt/spark/logs/stream.log')
 QUERY_LIST: List[StreamingQuery] = []
 
 if __name__ == "__main__":
-    spark_session = get_spark_session("Stream Silver Job")
+    spark_session = get_spark_session("Stream Job")
+    spark_session.sparkContext.setLogLevel("DEBUG")
+
     initialize_namespace(spark_session, 'silver', is_drop=True)
     initialize_namespace(spark_session, 'gold', is_drop=True)
 
@@ -23,7 +25,7 @@ if __name__ == "__main__":
 
     for job_class in job_class_list:
         job_instance:BaseJob = job_class(spark_session)
-        QUERY_LIST += [job_instance.get_query(process_time='10 seconds')]
+        QUERY_LIST += [job_instance.get_query(process_time='20 seconds')]
 
     # TODO: inference
     # review_comment = review_stream_df.select('review_id', 'review_comment_title', 'review_comment_message').dropna()
