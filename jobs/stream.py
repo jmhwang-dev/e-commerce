ce32678cef50358c50fc9aa11eb6f2386e590930
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-11-07 20:14:29 +0900

    fix(script): reset topics for silver  when run stream

--- jobs/stream.py
+++ jobs/stream.py
@@ -6,6 +6,8 @@ from service.utils.iceberg import init_catalog
 from service.utils.logger import *
 from service.pipeline.stream.silver import *
 from service.pipeline.stream.gold import *
+from service.utils.kafka import delete_topics, create_topics, get_confluent_kafka_admin_client
+from config.kafka import BOOTSTRAP_SERVERS_INTERNAL
 
 LOGGER = get_logger(__name__, '/opt/spark/logs/stream.log')
 
@@ -23,12 +25,16 @@ def run_stream(job_class_list):
 
 if __name__ == "__main__":
     spark_session = get_spark_session("stream")
+    admin_client = get_confluent_kafka_admin_client(BOOTSTRAP_SERVERS_INTERNAL)
+    silver_avsc_filenames = SilverAvroSchema.get_all_filenames()
+    delete_topics(admin_client, silver_avsc_filenames)
+    create_topics(admin_client, silver_avsc_filenames)
     init_catalog(spark_session, 'gold', is_drop=True)
 
     # TODO: inference
     # review_comment = review_stream_df.select('review_id', 'review_comment_title', 'review_comment_message').dropna()
     silver_job = [GeoCoordStream, OrderEventStream, CustomerOrderStream, ReviewMetadataStream, OlistUserStream, ProductMetadataStream]
     gold_job = [DimUserLocationStream, FactOrderTimelineStream, OrderDetailStream]
-
+    
     job_class_list:List[BaseStream] = silver_job + gold_job
     run_stream(job_class_list)
