7a3c082544a91c40427270c6955ee72c5f2af63c
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-11-07 20:16:35 +0900

    fix(script): initialize watermark table when run batch processing at first and modify batch test code

--- jobs/batch_test.py
+++ jobs/batch_test.py
@@ -1,40 +1,47 @@
+import time
+
 from typing import List
 from service.utils.iceberg import init_catalog
 from service.utils.spark import get_spark_session
 from service.pipeline.batch import base, silver, gold
+from service.utils.schema.reader import AvscReader
+from service.utils.schema.avsc import SilverAvroSchema
 
 if __name__ == "__main__":
-    spark_session = get_spark_session("Batch test", dev=True)
+    spark_session = get_spark_session("Batch", dev=True)
+    
+    init_catalog(spark_session, 'silver', is_drop=False)
+    init_catalog(spark_session, 'gold', is_drop=False)
 
-    init_catalog(spark_session, 'silver', is_drop=True)
-    init_catalog(spark_session, 'gold', is_drop=True)
+    watermark_avsc_reader = AvscReader(SilverAvroSchema.WATERMARK)
+    watermark_scheam = base.BaseBatch.get_schema(spark_session, watermark_avsc_reader)
+    base.BaseBatch.initialize_dst_table(spark_session, watermark_scheam, watermark_avsc_reader.dst_table_identifier)
 
     job_list: List[base.BaseBatch] = [
-        silver.GeoCoordBatch(),
-        silver.ReviewMetadataBatch(),
-        silver.OlistUserBatch(),
-        silver.OrderEventBatch(),
-        silver.CustomerOrderBatch(),
-        silver.ProductMetadataBatch(),
-
-        gold.DimUserLocationBatch(),
-        gold.FactOrderTimelineBatch(),
-        gold.OrderDetailBatch(),
-        gold.FactReviewStatsBatch(),
-        gold.FactOrderLeadDaysBatch(),
-        gold.FactProductPeriodSalesBatch(),
-        gold.FactProductPeriodPortfolioBatch()
+        silver.GeoCoordBatch(spark_session),
+        silver.OlistUserBatch(spark_session),
+        silver.ReviewMetadataBatch(spark_session),
+        silver.OrderEventBatch(spark_session),
+        silver.CustomerOrderBatch(spark_session),
+        silver.ProductMetadataBatch(spark_session),
+        gold.DimUserLocationBatch(spark_session),
+        gold.FactOrderTimelineBatch(spark_session),
+        gold.OrderDetailBatch(spark_session),
+        gold.FactReviewStatsBatch(spark_session),
+        gold.FactOrderLeadDaysBatch(spark_session),
+        gold.FactProductPeriodSalesBatch(spark_session),
+        gold.FactProductPeriodPortfolioBatch(spark_session)
     ]
 
-    i = 0
-    end = 1
-
-    while i < end:
+    schedule_interval = 0
+    try:
+        while True:
+            for job_instance in job_list:
+                job_instance.extract()
+                job_instance.transform()
+                job_instance.load()
+            time.sleep(schedule_interval)
+            print('='*80)
+    except KeyboardInterrupt:
         for job_instance in job_list:
-            job_instance.extract()
-            job_instance.transform()
-            job_instance.load()
-        i += 1
-
-    for job_instance in job_list:
-        job_instance.spark_session.stop()
+            job_instance.spark_session.stop()
