1658af7f416d829accf5068ecf01a1a2df34a7a6
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-12-02 21:52:01 +0900

    fix(kafka): change compose file name

--- src/config/confluent.py
+++ src/config/confluent.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 SCHEMA_DIR = Path('infra/confluent/schemas/')
 SCHEMA_REGISTRY_INTERNAL_URL = "http://schema-registry:8081"
-SCHEMA_REGISTRY_EXTERNAL_URL = "http://localhost:8082"
+SCHEMA_REGISTRY_EXTERNAL_URL = "http://192.168.45.192:8082"
 HEADERS = {
     "Accept": "application/vnd.schemaregistry.v1+json",
     "Content-Type": "application/vnd.schemaregistry.v1+json"
