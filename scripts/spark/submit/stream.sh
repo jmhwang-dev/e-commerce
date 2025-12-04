8a9d0f308fefce2fa1e8d1e7e984abd03d576223
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-12-04 16:27:02 +0900

    fix(kafka): add jmx_exporter jar and modify path

--- scripts/spark/submit/stream.sh
+++ scripts/spark/submit/stream.sh
@@ -48,8 +48,8 @@ docker cp "$SRC_ZIP" spark-client:/opt/spark/work-dir/$SRC_ZIP
 # Spark 실행
 docker compose -f docker-compose.spark-control-plane.yml exec spark-client spark-submit \
   --master spark://192.168.45.192:7078 \
-  --conf spark.driver.extraJavaOptions="-Daws.region=us-east-1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9082 -javaagent:/mnt/jmx_exporter/jmx_prometheus_javaagent-1.3.0.jar=9080:/mnt/configs/jmx/spark_driver.yml" \
-  --conf spark.executor.extraJavaOptions="-Daws.region=us-east-1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9082 -javaagent:/mnt/jmx_exporter/jmx_prometheus_javaagent-1.3.0.jar=9080:/mnt/configs/jmx/spark_executor.yml" \
+  --conf spark.driver.extraJavaOptions="-Daws.region=us-east-1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9082 -javaagent:/mnt/shared/jmx_prometheus_javaagent-1.3.0.jar=9080:/mnt/configs/jmx/spark_driver.yml" \
+  --conf spark.executor.extraJavaOptions="-Daws.region=us-east-1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9082 -javaagent:/mnt/shared/jmx_prometheus_javaagent-1.3.0.jar=9080:/mnt/configs/jmx/spark_executor.yml" \
   --deploy-mode client \
   --py-files /opt/spark/work-dir/$SRC_ZIP \
   "$PYTHON_SCRIPT"
