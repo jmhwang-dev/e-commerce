213dcfece09a1a9eece88873a4975a996ca71d73
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-12-04 14:33:44 +0900

    fix(cdc): modify script for raspberry pi to up and down

---
+++ scripts/run/raspberry/down.sh
@@ -0,0 +1,3 @@
+docker compose \
+    -f docker-compose.spark-worker-cdc.yml \
+    down -v
---
+++ scripts/run/raspberry/up.sh
@@ -0,0 +1,3 @@
+docker compose \
+    -f docker-compose.spark-worker-cdc.yml \
+    up -d --force-recreate
