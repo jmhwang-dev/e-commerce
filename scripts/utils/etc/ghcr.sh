77aa9208f64e33449f4bb6964092e5bcdeed88fc
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2025-12-11 14:43:20 +0900

    fix(script): modify script for ghcr and rm unnecessary script

--- scripts/utils/etc/ghcr.sh
+++ scripts/utils/etc/ghcr.sh
@@ -2,7 +2,10 @@ docker buildx create --name multi-builder --driver docker-container --use
 docker run --privileged --rm tonistiigi/binfmt --install all
 bash scripts/utils/build_image/warehouse.sh
 
-# export CR_PAT=사용자_토큰_값
-# echo $CR_PAT | docker login ghcr.io -u 깃허브_아이디 --password-stdin
-# docker tag my-app:latest ghcr.io/user123/my-app:v1.0
-# docker push ghcr.io/user123/my-app:v1.0
+
+export CR_PAT=
+echo $CR_PAT | docker login ghcr.io -u jmhwang-dev --password-stdin
+
+# push spark image for warehouse
+docker tag warehouse:sp3.5.6-ice1.9.1 ghcr.io/jmhwang-dev/warehouse:sp3.5.6-ice1.9.1
+docker push ghcr.io/jmhwang-dev/warehouse:sp3.5.6-ice1.9.1
