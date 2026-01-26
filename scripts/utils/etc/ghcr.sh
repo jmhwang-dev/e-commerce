1acc052d5f6cba8694ea11fae9eede8e4c7c4e00
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2026-01-26 17:46:46 +0900

    chore: add comment

--- scripts/utils/etc/ghcr.sh
+++ scripts/utils/etc/ghcr.sh
@@ -1,6 +1,7 @@
 docker buildx create --name multiarch --driver docker-container --bootstrap --use || docker buildx use multiarch
 docker run --privileged --rm tonistiigi/binfmt --install all
 
+# add token for ghcr.io
 export CR_PAT= && echo $CR_PAT | docker login ghcr.io -u jmhwang-dev --password-stdin
 
 DOCKER_BUILDKIT=1 docker buildx build \
